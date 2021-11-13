477efa173bb03e3972a0a11d62931a8c83f964a0
Morvin-Ian <morvinian@gmail.com>
2021-11-13 20:29:27 +0300

    mig

--- DarajaApp/admin.py
+++ DarajaApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TransactionDetails
+from .models import TransactionDetails,LNMOnline
 
 admin.site.register(TransactionDetails)
+admin.site.register(LNMOnline)
 
 
