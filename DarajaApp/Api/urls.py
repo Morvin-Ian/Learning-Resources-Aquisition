c7fe3845019508860a30d21a141c6dd3e3fdcab1
Morvin-Ian <morvinian@gmail.com>
2021-11-14 04:43:24 +0300

    mike

--- DarajaApp/Api/urls.py
+++ DarajaApp/Api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from DarajaApp.Api.views import LnmApiGenericView
+from DarajaApp.views import updatephone
 
 
 
 urlpatterns = [
 
     path('lnm/',LnmApiGenericView.as_view(), name='generics'),
+    path('phone/',LnmApiGenericView.as_view(), name='phone'),
     
   
 ]
