24608db72c165712010b7167a4648e52f765bcf0
Morvin-Ian <morvinian@gmail.com>
2021-11-14 03:35:17 +0300

    mile

--- DarajaApp/Api/views.py
+++ DarajaApp/Api/views.py
@@ -3,18 +3,13 @@ from .serializers import LNMOnlineSerializer
 
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-
-
-
+from rest_framework.permissions import AllowAny
   
 class LnmApiGenericView(CreateAPIView):
     queryset = LNMOnline.objects.all()
     serializer_class = LNMOnlineSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
+   
 
     def create(self,request):
         print(request.data,"Successfull Morvin")
